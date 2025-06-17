e76d20cdfd4e48b2828284cd28457154161c9270
Aliasgar777 <aliasgarforwork@gmail.com>
2025-06-17 11:02:47 +0530

    improving security in service.py file

--- app/auth/service.py
+++ app/auth/service.py
@@ -1,6 +1,10 @@
 import smtplib
 from email.message import EmailMessage
 from app.core.logger import logger
+from dotenv import load_dotenv
+import os
+load_dotenv()
+APPLICATION_PASSWORD = os.getenv("APPLICATION_PASSWORD")
 
 def send_reset_email(email: str, token: str):
     reset_link = f"http://127.0.0.1:8000/token={token}"
@@ -13,5 +17,5 @@ def send_reset_email(email: str, token: str):
     logger.info(f"email sent for reset password to {email}")
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.starttls()
-        smtp.login("aliasgarsaify61@gmail.com", "oazjcqbljgfyobwk")
+        smtp.login("aliasgarsaify61@gmail.com", APPLICATION_PASSWORD)
         smtp.send_message(msg)
